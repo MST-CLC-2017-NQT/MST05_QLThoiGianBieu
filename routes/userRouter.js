77f9b010e0e7350af73c264b7e0a8136e8618ca4
tukida <tuankiet.hcmc@gmail.com>
2017-10-13 11:57:26 +0700

    edit server.ejs

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -7,44 +7,44 @@ var express = require('express'),
 var User = require('../models/user');
 var csrf = require('csurf'),
     csrfProtection = csrf();
-    
-router.use(bodyParser.urlencoded({extended: true}));
+
+router.use(bodyParser.urlencoded({ extended: true }));
 router.use(csrfProtection);
 
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost/MST05_QLThoiGianBieu";
 
 router.route('/')
-.get((req, res) => {
-res.render('home.ejs');
-});
+    .get((req, res) => {
+        res.render('home.ejs');
+    });
 
 router.get('/register', function(req, res, next) {
-var messages = req.flash('error');
-res.render('register.ejs',  {csrfToken: req.csrfToken(), messages: messages});
+    var messages = req.flash('error');
+    res.render('register.ejs', { csrfToken: req.csrfToken(), messages: messages });
 });
 
 router.post('/register', passport.authenticate('local.signup', {
-successRedirect: '/login',
-failureRedirect: '/register',
-failureFlash: true
+    successRedirect: '/login',
+    failureRedirect: '/register',
+    failureFlash: true
 }));
 
 router.get('/login', (req, res) => {
-var messages = req.flash('error');
-res.render('login.ejs',  {csrfToken: req.csrfToken(), messages: messages});
+    var messages = req.flash('error');
+    res.render('login.ejs', { csrfToken: req.csrfToken(), messages: messages });
 });
 
 router.post('/login', passport.authenticate('local.signin', {
-successRedirect : '/', 
-failureRedirect : '/login', 
-failureFlash : true 
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
 }));
 
 router.get('/addEvent', (req, res) => {
-var messages = req.flash('error');
-res.render('addEvent.ejs', {messages: 'nqt'});
-//res.render('./ejs/addEvent.ejs', {csrfToken: req.csrfToken(), messages: messages});
+    var messages = req.flash('error');
+    res.render('addEvent.ejs', { messages: 'nqt' });
+    //res.render('./ejs/addEvent.ejs', {csrfToken: req.csrfToken(), messages: messages});
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,18 +1,20 @@
- var express = require('express');
-var app = express();
+var express = require('express'),
+    morgan = require('morgan'),
+    session = require('express-session'),
+    mongoose = require('mongoose'),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser'),
+    ejs = require('ejs'),
+    flash = require('connect-flash'),
+    passport = require('passport'),
+    validator = require('express-validator'),
+    MongoStore = require('connect-mongo')(session),
+    configDB = require('./config/database.js');
+
 var port = 8080;
+var app = express();
 var router = express.Router();
-var morgan = require('morgan');
-var session = require('express-session');
-var mongoose = require('mongoose');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var ejs = require('ejs');
-var flash = require('connect-flash');
-var passport = require('passport');
-var validator = require('express-validator');
-var MongoStore = require('connect-mongo')(session);
-var configDB = require('./config/database.js');
+
 require('./config/passport');
 //database config:
 mongoose.connect(configDB.url);
@@ -22,26 +24,33 @@ app.use(cookieParser());
 app.use(session({
     secret: 'anystringtotext',
     resave: true,
-    saveUninitialized: true, 
-    store: new MongoStore({mongooseConnection: mongoose.connection}),
-    cookie: {maxAge: 180 * 60 * 1000}          
+    saveUninitialized: true,
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+    cookie: { maxAge: 180 * 60 * 1000 }
 }));
 app.use(function(req, res, next) {
     res.session = req.session;
-    next();   
+    next();
 });
-app.use(bodyParser.urlencoded({extended: true}));
-app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(flash());
 app.use(validator());
 app.use(passport.initialize());
 app.use(passport.session());
-//
+app.set('view engine', 'ejs');
+// CSS
 app.use(express.static('stylesheets'));
 // Routes
-app.use('/', require('./routes/userRouter'));
-app.use('/', require('./routes/eventRouter'));
-
+app.use('/user', require('./routes/userRouter'));
+app.use('/event', require('./routes/eventRouter'));
+// listen port 8080
 app.listen(port, () => {
     console.log("Listening on port: " + port);
 });
+
+//get home page
+app.get('/', (req, res) => {
+    ejs.renderFile('./views/home.ejs', {}, (err, html) => {
+        res.end(html);
+    });
+})
