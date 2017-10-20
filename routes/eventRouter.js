1e5b413bf297dc62c878ad59d226ee33aad3e194
= <=>
2017-10-20 20:50:40 +0700

    validator backend

--- routes/eventRouter.js
+++ routes/eventRouter.js
@@ -9,13 +9,13 @@ var ObjectID = mongoose.Types.ObjectId;
 router.use(bodyParser.urlencoded({ extended: true }));
 router.use(csrfProtection);
 var User = require('../models/user');
+const { check, oneOf, validationResult } = require('express-validator/check');
 
 function handleError(res, reason, message, code) {
     console.log("ERROR: " + reason);
     res.status(code || 500).json({ "error": message });
 }
 //GET list event:
-
 router.get('/list', (req, res) => {
     var uid = req.session.uid;
     User.find({_id:uid}, (err,data) =>{
@@ -36,13 +36,13 @@ router.get('/list', (req, res) => {
     // });  
     
 });
-
 //GET add event page:
 router.get('/', (req, res) => {
-    res.render('addEvent.ejs', {csrfToken: req.csrfToken(),messages: ''});
+    res.render('addEvent.ejs', {csrfToken: req.csrfToken(),messages: ' '});
 });
 
 //POST an event:
+
 router.post('/', (req, res) => {
     //var user = new User();
     var uid = req.session.uid;
@@ -52,33 +52,43 @@ router.post('/', (req, res) => {
     let date = req.body.date;
     let location = req.body.location;
     let priority = req.body.priority;
+    req.checkBody('name', 'Name: only accept letter and number(A-Z, 0-9), length 5-20').isLength({min: 5, max: 20}).matches(/[A-Z0-9_]+$/);
+    req.checkBody('description', 'Description: only accept length 5-30').isLength({min: 5, max: 30});
+    req.checkBody('location', 'Location: only accept length 2-20').isLength({min: 2, max: 20});
+    var messages = [];
+    var errValidate = req.validationErrors();
     var updateDoc = { id: new ObjectID(), name: name, description: description, date: date, location: location, priority: "important" };
-    User.update({ _id: new ObjectID(uid) }, { '$push': { 'events': updateDoc } }, function (err, doc) {
-        if (err) {
-            User.update({ _id: new ObjectID(uid) }, { '$set': { 'events': [updateDoc] } }, function (err, doc) {
-                if (err) {
-                    handleError(res, err.message, "Failed to update student");
-                } else {
-                    res.status(200).json(doc);
-                }
-            });
-        } else {
-            res.redirect('/event');
-            
-        }
-    });
-
-
+    if(errValidate) {                
+        errValidate.forEach(function(error) {
+            messages.push(error.msg);
+        });
+        res.render('addEvent.ejs', {csrfToken: req.csrfToken(), messages: messages});
+    } else {
+        User.update({ _id: new ObjectID(uid) }, { '$push': { 'events': updateDoc } }, function (err, doc) { 
+            if (err) {
+    
+                User.update({ _id: new ObjectID(uid) }, { '$set': { 'events': [updateDoc] } }, function (err, doc) {
+                    if (err) {
+                        handleError(res, err.message, "Failed to update student");
+                    } else {
+                        res.status(200).json(doc);
+                    }
+                });
+            } else {
+                res.redirect('/event');
+                
+            }
+        });
+    }
 });
 
 //PUT an event:
 router.put('/:id', (req, res) => {
-    //var id = req.params.id;
-    var id = req.session.id;
-    console.log(id);
+    var uid = req.session.uid;
+    console.log(uid);
     mongoose.connect(configDB.url, function () {
         db.collection("users").findAndModify({
-            query: { _id: mongojs.ObjectId(id) },
+            query: { id: mongojs.ObjectId(uid) },
             update:
             {
                 $set:
@@ -99,14 +109,22 @@ router.put('/:id', (req, res) => {
 
 //DELETE an event:
 router.delete('/:id', (req, res) => {
-    var id = req.params.id;
+    var id = req.session.id;
     console.log(id);
-    mongoose.connect(configDB.url, function () {
-        db.collection("users").remove({ _id: mongojs.ObjectId(id) }, function (err, doc) {
-            res.json(doc);
-            //res.redirect("");
-        });
-    });
+    User.findOne({id: mongoose.Types.ObjectId(id)}, function(err, doc) {
+        if(err) {
+            handleError(res, err.message, "Failed to delete event.");
+        } 
+        if(doc) {
+            doc.remove(function (err) {
+                if(err) {
+                    handleError(res, err.message, "Failed to delete event.");
+                } else {
+                    res.status(200).json(id);
+                }
+            });
+        } 
+    });    
 });
 
 module.exports = router;
